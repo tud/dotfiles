fdd1218dd837ff05f00c0c86e8e1e8f93aa62ee8
stefano <stefano@manieri.info>
2012-05-11 18:01:38 +0200

    insert file per update git e/o subversion

---
+++ upd.sh
@@ -0,0 +1,21 @@
+clean="NO"
+if [ $# -eq 1 ]; then
+  if [ $1 = "-c" ]; then
+    clean="YES"
+  fi
+fi
+dirs=`find . -type d -depth 1`
+for dit in $dirs
+do
+  echo $dit
+  cd $dit
+  if [ -d ".svn" ]; then
+    svn update
+  elif [ -d ".git" ]; then
+    if [ $clean = "YES" ]; then
+      git gc
+    fi
+    git pull
+  fi 
+  cd ..
+done
